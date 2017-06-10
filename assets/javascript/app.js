869f108a46c099038f1109b67072e2dd1eea8a45
Derryck Kamptapersaud <Derryck Kamptapersaud>
2017-06-10 10:49:53 -0400

    javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+function countdown(){
+	seconds = 60;
+	$(".timer").html('<h3>Time Remaining: ' + seconds + '</h3>');
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$(".timer").html('<h3>Time Remaining: ' + seconds + '</h3>');
+    if(seconds < 1){
+		clearInterval(time);
+    }
+}
+
+countdown();
+
+  function displayPlayerInfo() {
+        
+      var queryURL = "https://www.fantasyfootballnerd.com/service/auction/json/9azppczwsynp/";
+      
+			$.ajax({
+      url: queryURL,
+      method: "GET",
+      }).done(function(response) {
+      console.log(response);
+			 for (var i = 0; i < response.AuctionValues.length; i++);
+			 console.log(i);
+			 
+	
+			});
+		}
+	displayPlayerInfo();
